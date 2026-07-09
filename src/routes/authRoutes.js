bc0cea2c129d39f909d4a638bbbe00e9c3235ab8
zahra <zahraaamir000@gmail.com>
2026-07-09 17:12:22 +0500

    code updayed

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login, getMe } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 const { signup, login, getMe, updateMeasurements } = require('../controllers/authController');
 
